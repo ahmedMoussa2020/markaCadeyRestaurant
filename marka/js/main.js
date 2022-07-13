95f48177beea25684609cb37c3f32744a2b00a35
Ahmed <>
2022-07-13 19:19:18 -0400

    scroll top

--- marka/js/main.js
+++ marka/js/main.js
@@ -1,5 +1,4 @@
 // SHOW MENU    
-
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -13,30 +12,27 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle', 'nav-menu')
 
+
 // REMOVE MENU MOBILE
-
 const navLink = document.querySelectorAll('.link__link')
-
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     //when we click on each nav_link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
-
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+
 // SCROLL SECTIONS ACTIVE LINK
-
 const sections = document.querySelectorAll('section[id]')
-
 
 function scrollActive(){
     const scrollY = window.pageXOffset
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop
+        const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
@@ -46,16 +42,14 @@ function scrollActive(){
         }
     })
 }
-
 window.addEventListener('scroll', scrollActive)
 
 
 // CHANGE BACKGROUND HEADER
-
 function scrollHeader(){
     const nav = document.getElementById('header')
     //when the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 200) nav.classList.add('scroll', scrollHeader); else nav.classList.remove('scroll-header')
+    if(this.scrollY >= 200) nav.classList.add('scroll-header', scrollHeader); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
